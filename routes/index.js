085da4514e3877a62b6269bf1ccc43c1948cb77d
patrick cremin <pcremin@us.ibm.com>
2017-03-19 20:18:11 -0500

    fixed bug

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,20 @@ var DiscoveryV1 = require( 'watson-developer-cloud/discovery/v1' );
 function getDiscoveryCredentials()
 {
 	var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+	
 	// Pattern match to find the first instance of a Discovery service in
 	// VCAP_SERVICES. If you know your service key, you can access the
 	// service credentials directly by using the vcapServices object.
-	for (var vcapService in vcapServices) {
+	for (var vcapService in vcapServices) 
+	{
 		if (vcapService.match(/discovery/i)) 
 		{
 		        return {
 		        		username: vcapServices[vcapService][0].credentials.username,
 						password: vcapServices[vcapService][0].credentials.password
 				};
-		}		
+		}
+	}
 }
 
 
